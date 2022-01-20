a59b73ca25eb277725beafe1a123f2c584b2b4d3
Amodu Kehinde <kennisdamilare@gmail.com>
2022-01-20 02:29:14 +0100

    first heroku web now

--- app.js
+++ app.js
@@ -19,12 +19,10 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, '/client/build')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
-    });
-}
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
+}) 
 
 app.use(bodyParser.json());
 app.use(cookieParser()) 
