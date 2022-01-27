4dcc274386b2fcd38b310f5cec3ccbaf4cf93930
Amodu Kehinde <kennisdamilare@gmail.com>
2022-01-27 04:19:47 +0100

    heroku push

--- app.js
+++ app.js
@@ -23,15 +23,16 @@ app.use(express.json());
 
 // app.use(express.static(path.join(__dirname, '/client/build')));
 // app.get('*', (req, res) => {
-// 	res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+// 	 res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// 	// res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 // });
 
-if (process.env.NODE_ENV === "production") { 
+
 	 app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-}
+
 
 
 app.use(bodyParser.json());
--- server.js
+++ server.js
@@ -12,14 +12,14 @@ process.on('uncaughtException', (err) => {
 
 dotenv.config({ path: './config.env' });
 
-// const DB = process.env.DATABASE.replace(
-//   'PASSWORD',
-//   process.env.DATABASE_PASSWORD
-// );
+const DB = process.env.DATABASE.replace(
+  'PASSWORD',
+  process.env.DATABASE_PASSWORD
+);
 
 mongoose
-	.connect(process.env.DATABASE_LOCAL, {
-		// .connect(DB, {
+	// .connect(process.env.DATABASE_LOCAL, {
+		.connect(DB, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 		useFindAndModify: false,
