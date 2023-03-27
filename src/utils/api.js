da93424139538bff0ac81c9c2ae547f514947986
Amodu Kehinde <amodukehindee@gmail.com>
2023-03-27 09:27:04 +0100

    add new api host

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:7000/api",
+  baseURL: "https://purple-buffalo-wear.cyclic.app/api",
   // baseURL: "https://beardmen-store-api.herokuapp.com/api",
   headers: {
     "Content-type": "application/json"
